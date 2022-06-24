d485809e09cb8f5e3755de83503cf885535468b0
Tim Morello <tdmorello@gmail.com>
2022-06-24 10:49:07 -0400

    Add more sample data

--- src/napari_basicpy/_sample_data.py
+++ src/napari_basicpy/_sample_data.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
 
+import basicpy.data
 import numpy as np
 
 if TYPE_CHECKING:
     import napari
 
+# TODO logging?
 
-def make_sample_data() -> List[napari.types.LayerData]:
+
+def make_sample_data_random() -> List[napari.types.LayerData]:
     """Generate a parabolic gradient to simulate uneven illumination"""
     np.random.seed(42)
 
@@ -19,16 +22,38 @@ def make_sample_data() -> List[napari.types.LayerData]:
     grid = np.meshgrid(*(2 * (np.linspace(-size // 2 + 1, size // 2, size),)))
 
     # Create the gradient (flatfield) with and offset (darkfield)
-    gradient = sum(d ** 2 for d in grid) ** (1 / 2) + 8
+    gradient = sum(d**2 for d in grid) ** (1 / 2) + 8
     gradient_int = gradient.astype(np.uint8)  # type: ignore
 
     # Create an image stack and add poisson noise
-    images = np.random.poisson(lam=gradient_int.flatten(), size=(n_images, size ** 2))
+    images = np.random.poisson(lam=gradient_int.flatten(), size=(n_images, size**2))
     images = images.transpose().reshape((size, size, n_images))
     images = np.moveaxis(images, -1, 0)
     images = 255 - images
 
-    return [(images, {"name": "Uneven Illumination"}, "image")]
+    return [(images, {"name": "Random"}, "image")]
 
 
-make_sample_data()
+def make_sample_data_cell_culture() -> List[napari.types.LayerData]:
+    data = basicpy.data.cell_culture()
+    return [(data, {"name": "Cell Culture"}, "image")]
+
+
+def make_sample_data_timelapse_brightfield() -> List[napari.types.LayerData]:
+    data = basicpy.data.timelapse_brightfield()
+    return [(data, {"name": "Timelapse Brightfield"}, "image")]
+
+
+def make_sample_data_timelapse_nanog() -> List[napari.types.LayerData]:
+    data = basicpy.data.timelapse_nanog()
+    return [(data, {"name": "Timelapse Nanog"}, "image")]
+
+
+def make_sample_data_timelapse_pu1() -> List[napari.types.LayerData]:
+    data = basicpy.data.timelapse_pu1()
+    return [(data, {"name": "Timelapse Pu1"}, "image")]
+
+
+def make_sample_data_wsi_brain():
+    data = basicpy.data.wsi_brain()
+    return [(data, {"name": "WSI Brain"}, "image")]
--- src/napari_basicpy/_tests/test_sample_data.py
+++ src/napari_basicpy/_tests/test_sample_data.py
@@ -1,8 +1,24 @@
+"""Test sample data."""
+
+
 def test_data(make_napari_viewer):
+    samples = [
+        "sample_data_random",
+        "sample_data_cell_culture",
+        "sample_data_timelapse_brightfield",
+        "sample_data_timelapse_nanog",
+        "sample_data_timelapse_pu1",
+        "sample_data_wsi_brain",
+    ]
 
     viewer = make_napari_viewer()
-    n = len(viewer.layers)
-    viewer.open_sample("napari-basicpy", "sample_data")
-    assert len(viewer.layers) == (n + 1)
+    for sample in samples:
+        n = len(viewer.layers)
+        viewer.open_sample(
+            "napari-basicpy",
+            sample,
+        )
+        assert len(viewer.layers) == (n + 1)
+
     # NOTE
     # assert viewer.layers[0].dtype == np.uint8
